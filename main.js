258e040c6c38d15cd2efae586ed5a0b4848f7134
Micael_Henrique <133661765+Mikhailcherniev@users.noreply.github.com>
2023-10-03 11:14:05 -0300

    finalização
    q

--- main.js
+++ main.js
@@ -1,50 +1,49 @@
-// Definindo a classe contatos
 class Contatos {
-    constructor(nome, email, telefone, mensagem){
+    constructor(
+        nome, email, telefone, mensagem
+    ) {
         this.nome = nome;
         this.email = email;
         this.telefone = telefone;
-        this.mensagem = mensagem;
+        this.mensagem = mensagem
     }
 }
 
-// Definindo a classe GerenciadorContatos
 class GerenciadorContatos {
-    constructor(){
-        // Inicializa a lista de contatos vazia
+    constructor() {
+        //inicializa lista de contatos vazia
         this.contatos = []
     }
 
     adicionarContato(contato) {
-        // Adiciona um contato a lista de contatos
-        this.contatos.push(contato);
+        //add contato a lista
+        this.contatos.push(contato)
     }
 
     exibirContatos() {
-        // Obtem a lista de contatos
+        //obtem lista de contatos
         const listaContatos = document.getElementById('contato-lista');
 
-        //Limpa a lista de contatos
+        //limpa lista de contatos
         listaContatos.innerHTML = '';
 
-        // Para cada contato na lista de contatos, cria um novo elemento 'li' e adiciona na lista de contatos
-        for(const contato of this.contatos) {
+        //
+        for (const contato of this.contatos) {
             const li = document.createElement('li');
-            li.innerHTML = `${contato.nome} - ${contato.email} - ${contato.telefone}`;
-            listaContatos.appendChild(li)
+            li.innerHTML = `${contato.nome} <br> ${contato.email}<br>  ${contato.telefone} <br><br> ${contato.mensagem}`;
+            listaContatos.appendChild(li);
         }
     }
 }
 
-// Cria um objeto de classe GerenciadorContatos
 const gerenciadorContatos = new GerenciadorContatos();
 
-// Cria os elementos necessarios
+//cria elements necessarios
 const contatoForm = document.getElementById('contato-form');
 const nomeF = document.getElementById('nome');
 const emailF = document.getElementById('email');
 const telefoneF = document.getElementById('telefone');
-const  mensagemF = document.getElementById('mensagem');
+const mensagemF = document.getElementById('mensagem');
 const mostrarContatos = document.getElementById('mostrar-contatos');
 const ocultarContatos = document.getElementById('ocultar-contatos');
 const listaContatos = document.getElementById('contato-lista');
@@ -55,10 +54,10 @@ contatoForm.addEventListener('submit', (event) => {
     const nome = nomeF.value;
     const email = emailF.value;
     const telefone = telefoneF.value;
-    const mensagem = mensagemF.value
+    const mensagem = mensagemF.value;
 
-    // Criando objeto da classe Contato
-    const contato = new Contatos(nome, email, telefone);
+    //Criando objeto  da classe Contato
+    const contato = new Contatos(nome, email, telefone, mensagem);
 
     gerenciadorContatos.adicionarContato(contato);
 
@@ -66,17 +65,14 @@ contatoForm.addEventListener('submit', (event) => {
     emailF.value = '';
     telefoneF.value = '';
     mensagemF.value = '';
-})
+});
 
-
-// Estamos adicionando um manipulador de eventos para o botão mostrar contatos, que exibe a lista de contatos
+//add manipulador de eventos para o botao exibir a lista de contatos
 mostrarContatos.addEventListener('click', () => {
     gerenciadorContatos.exibirContatos();
     listaContatos.style.display = 'block';
 })
 
-ocultarContatos.addEventListener('click', () =>{
-    listaContatos.style.display = 'none'
+ocultarContatos.addEventListener('click', ()=>{
+    listaContatos.style.display = 'none';
 })
-
-
